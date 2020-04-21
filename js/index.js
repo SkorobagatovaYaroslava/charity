c69ff394b3479a282762c23bf481e65440de0c8d
SkorobagatovaYaroslava <slavaskorobagatova@gmail.com>
2020-04-21 16:10:57 +0300

    made a section with statistics

--- js/index.js
+++ js/index.js
@@ -20,3 +20,17 @@ $(document).ready(function(){
     }
    })
 });
+
+/* for section Statistics */
+
+$('.Statistics__block-number').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});

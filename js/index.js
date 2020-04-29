89c468a16e955c864cd7934a93225a23037715d7
SkorobagatovaYaroslava <slavaskorobagatova@gmail.com>
2020-04-29 12:55:49 +0300

    made a section with a form, changed the font size for all title

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     initCarousel();
     runNumbersAnimation();
+    effectAware();
+
 });
 
 
@@ -38,5 +40,24 @@ function runNumbersAnimation() {
                 $(this).text(Math.ceil(now));
             }
         });
+    });
+}
+
+function effectAware() {
+    $('.contacts__form-button')
+        .on('mouseenter', function(e) {
+            var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+            $(this).find('span').css({top: relY, left: relX})
+        })
+        .on('mouseout', function(e) {
+            var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+            $(this).find('span').css({top: relY, left: relX})
+        });
+    $('[href=#]').click(function () {
+        return false
     });
 }
